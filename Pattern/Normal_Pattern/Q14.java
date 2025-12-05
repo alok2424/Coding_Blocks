dce46eda960ebe333590dfcb442d82240e8dea8f
alok2424 <22ec059@jssaten.ac.in>
2025-12-05 12:12:39 +0530

    "Pattern_Double_Sided_Arrow"

---
+++ Pattern/Normal_Pattern/Q14.java
@@ -0,0 +1,54 @@
+//PAttern_Double_Sided_arrow
+//R4-- pattern
+class Main {
+    public static void main(String[] args) {
+        int n = 4;
+        int row = 1;
+        int num = 1;
+        int space1 = 2*(n-row);
+        int space2 = -1;
+        while (row <= 2*n-1) {
+            //space
+            int  j = 1;
+            while(j<space1){
+                System.out.print("  ");
+                j++;
+            }
+            //num
+            int i = num;
+            while(i>=1){
+                System.out.print(i+" ");
+                i--;
+            }
+            //space_2
+            int l = 0;
+            while(l<space2){
+                System.out.print("  ");
+                l++;
+            }
+            //vertical mirror
+            int k = 1;
+            if(row == 1 || row == 2*n-1){
+                k = 2;
+            }
+            while(k<= num){
+                System.out.print(k+" ");
+                k++;
+            }
+            //horizontal mirror
+            if(row < n){
+                space1-=2;
+                num++;
+                space2+=2;
+            }else{
+                space1+=2;
+                num--;
+                space2-=2;
+            }
+            //next page
+            System.out.println();
+            row++;
+            
+        }
+    }
+}
