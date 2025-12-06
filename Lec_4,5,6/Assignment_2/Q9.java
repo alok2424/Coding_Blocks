588cb43eabf669fe043daf850e3dc03c1cd3b7d7
alok2424 <22ec059@jssaten.ac.in>
2025-12-06 07:00:14 +0530

    "GCD"

--- Lec_4,5,6/Assignment_2/Q9.java
+++ Lec_4,5,6/Assignment_2/Q9.java
@@ -1,1 +1,17 @@
 //GCD
+class GCD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int divisor = 60;
+		int dividend = 36;
+		while (dividend % divisor != 0) {
+			int rem = dividend % divisor;
+			dividend = divisor;
+			divisor = rem;
+		}
+		System.out.println(divisor);
+
+	}
+
+}
