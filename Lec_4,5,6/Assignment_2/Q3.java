2026b25efd7eb4e30f487e2a7cf2f3dc72461803
alok2424 <22ec059@jssaten.ac.in>
2025-12-06 06:05:43 +0530

    "reverse_num"

---
+++ Lec_4,5,6/Assignment_2/Q3.java
@@ -0,0 +1,17 @@
+//Reverse Num
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         System.out.println("enter the num ");
+        int num = sc.nextInt();
+        int rev = 0;
+         while(num>0){
+            int rem = num % 10;
+             rev = rev * 10 + rem;
+             num = num /10;
+         }
+       System.out.println("Reverse of num " + rev);
+        
+    }
+}
