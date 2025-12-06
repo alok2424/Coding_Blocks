9b099496d615933caff80abdc1dca3b988708446
alok2424 <22ec059@jssaten.ac.in>
2025-12-06 06:59:10 +0530

    "dec_to_Binary"

--- Lec_4,5,6/Assignment_2/Q7.java
+++ Lec_4,5,6/Assignment_2/Q7.java
@@ -13,3 +13,20 @@ class Bin_to_Dec {
 		System.out.println(sum);
 	}
 }
+ class Dec_to_Bin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 172;
+		int mul = 1;
+		int sum = 0;
+		while (n > 0) {
+			int rem = n % 2;
+			sum = sum + rem * mul;
+			n = n / 2;
+			mul = mul * 10;
+		}
+		System.out.println(sum);
+	}
+
+}
