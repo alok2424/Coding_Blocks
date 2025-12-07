640faa7823f67bb21ea4a97ae23b4f634c2f0302
alok2424 <22ec059@jssaten.ac.in>
2025-12-07 11:31:25 +0530

    "Inverse_Of_Num"

--- Lec_4,5,6/Assignment_2/Q10.java
+++ Lec_4,5,6/Assignment_2/Q10.java
@@ -1,6 +1,20 @@
-//Inverse of number
-class Main{
-    public static void main(String[] args){
-        
+import java.util.*;
+
+class Main {
+
+    public static int inverse(int n) {
+        int position = 1;
+        int inverse = 0;
+
+        while (n > 0) {
+            int digit = n % 10;  // current digit
+            // place "position" at index = digit
+            inverse += position * (int)Math.pow(10, digit - 1);
+
+            position++;
+            n /= 10;
+        }
+
+        return inverse;
     }
 }
