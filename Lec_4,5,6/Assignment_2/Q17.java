066a79e808ea2efecb6c0a9210c739af6acc7f69
alok2424 <22ec059@jssaten.ac.in>
2025-12-07 11:56:32 +0530

    "print_series"

---
+++ Lec_4,5,6/Assignment_2/Q17.java
@@ -0,0 +1,26 @@
+//Print-Series
+
+import java.util.*;
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N1 = sc.nextInt(); // how many terms to print
+        int N2 = sc.nextInt(); // skip multiples of this
+
+        int count = 0;
+        int n = 1;
+
+        while (count < N1) {
+            int val = 3 * n + 2;
+
+            if (val % N2 != 0) {   // print only if NOT divisible by N2
+                System.out.println(val);
+                count++;
+            }
+
+            n++;
+        }
+    }
+}
