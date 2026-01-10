2dcc39a2d6a2274a4b7b8259922e52abed898f96
alok2424 <22ec059@jssaten.ac.in>
2026-01-10 15:22:56 +0530

    "Backtrakking"

---
+++ Recursion/Backtrakking/Queen_Permutation.java
@@ -0,0 +1,21 @@
+class Main{
+    public static void main(String[] args){
+        int n = 4;
+        int tq = 2;
+        boolean[] board = new boolean[n];
+        Permutation(board,tq,0,"");
+    }
+    public static void Permutation(boolean[] board,int tq,int qpsf,String ans){
+        if(tq == qpsf){
+            System.out.println(ans);
+            return;
+        }
+        for(int i = 0; i < board.length; i++){
+            if(board[i] == false){
+                board[i] =true;
+                Permutation(board,tq,qpsf+1,ans+"b"+i+"q"+qpsf);
+                board[i] = false;//UNDO // backtrakking step
+            }
+        }
+    }
+}
