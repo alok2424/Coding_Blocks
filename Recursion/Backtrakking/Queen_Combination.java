5b0020f0793546ac8df84fb418af41540034e9f4
alok2424 <22ec059@jssaten.ac.in>
2026-01-14 12:57:03 +0530

    "Commit"

--- Recursion/Backtrakking/Queen_Combination.java
+++ Recursion/Backtrakking/Queen_Combination.java
@@ -3,9 +3,9 @@ class Main{
         int n = 4;
         int tq = 2;
         boolean[] board = new boolean[n];
-        Combination(board,tq,0,"");
+        Combination(board,tq,0,"",0);
     }
-    public static void Combination(boolean[] board,int tq,int qpsf,String ans){
+    public static void Combination(boolean[] board,int tq,int qpsf,String ans,int idx){
         if(tq == qpsf){
             System.out.println(ans);
             return;
@@ -13,7 +13,7 @@ class Main{
         for(int i = 0; i < board.length; i++){
             if(board[i] == false){
                 board[i] =true;
-                Combination(board,tq,qpsf+1,ans+"b"+i+"q"+qpsf);
+                Combination(board,tq,qpsf+1,ans+"b"+i+"q"+qpsf,i+1);//idx+1 ya i+1
                 board[i] = false;//UNDO // backtrakking step
             }
         }
