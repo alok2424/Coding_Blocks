99145f5fe0c59030cccfa5f8941501d1500c06f3
alok2424 <22ec059@jssaten.ac.in>
2026-01-17 00:27:00 +0530

    "commit"

--- String/151.java
+++ String/151.java
@@ -4,7 +4,7 @@ class Main{
         s = s.trim();
         String[] arr = s.split(" +");
         String ans= "";//blank string
-        for(int i = arr.length-1; i>0;i--){
+        for(int i = arr.length-1; i>=0;i--){
             ans  += arr[i] + " ";
         }
         ans  = ans.trim();
