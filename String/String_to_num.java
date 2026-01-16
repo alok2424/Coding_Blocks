7590a2d8cd6e00f573f4b0f718a6a735e524b99a
alok2424 <22ec059@jssaten.ac.in>
2026-01-16 23:52:16 +0530

    "commit"

---
+++ String/String_to_num.java
@@ -0,0 +1,11 @@
+class String_To_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="654789";
+		int i=Integer.parseInt(s);
+		Long l=Long.parseLong(s);
+
+	}
+
+}
